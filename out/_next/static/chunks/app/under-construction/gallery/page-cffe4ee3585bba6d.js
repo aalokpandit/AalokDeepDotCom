88e0a4d6570bc9739ecd06187f08583068dff806
aalok <aalokpandit@outlook.com>
2025-11-27 18:56:39 -0800

    chore: remove build artifacts from index

--- out/_next/static/chunks/app/under-construction/gallery/page-cffe4ee3585bba6d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[227,780,716,467,577],{7630:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=7630)}),_N_E=n.O()}]);
